c833ce52fb006df20f4853e3bca5eb1153eadb5f
Brandon Alexander <brandonwalex@pm.me>
2026-07-13 20:58:42 -0500

    Enable i686-linux binfmt registration for 32-bit x86 emulation

--- configuration.nix
+++ configuration.nix
@@ -31,11 +31,19 @@ in
     extraModprobeConfig = ''
       options hid_apple iso_layout=0
     '';
-    binfmt.registrations.x86_64-linux = {
-      recognitionType = "magic";
-      magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'';
-      mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
-      interpreter = "${muvm-fex-wrapper}/bin/muvm-fex-wrapper";
+    binfmt.registrations = {
+      x86_64-linux = {
+        recognitionType = "magic";
+        magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'';
+        mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
+        interpreter = "${muvm-fex-wrapper}/bin/muvm-fex-wrapper";
+      };
+      i686-linux = {
+        recognitionType = "magic";
+        magicOrExtension = ''\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x03\x00'';
+        mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
+        interpreter = "${muvm-fex-wrapper}/bin/muvm-fex-wrapper";
+      };
     };
   };
 
